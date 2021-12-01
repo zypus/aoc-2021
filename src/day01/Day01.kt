96578a35a9435426655072801aeaac8bedb23fb4
Fabian <7341484+zypus@users.noreply.github.com>
2021-12-01 11:43:12 +0100

    Solution day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -2,17 +2,26 @@ package day01
 
 import AoCTask
 
+// https://adventofcode.com/2021/day/1
+
 fun part1(input: List<String>): Int {
-    return input.size
+    return input.map(String::toInt).windowed(2) {
+        it[0] < it[1]
+    }.count { increased -> increased }
 }
 
 fun part2(input: List<String>): Int {
-    return input.size
+    return input.map(String::toInt).windowed(3, 1) {
+        it.sum()
+    }.windowed(2) {
+        it[0] < it[1]
+    }.count { increased -> increased }
 }
 
 fun main() = AoCTask("day01").run {
     // test if implementation meets criteria from the description, like:
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     println(part1(input))
     println(part2(input))
