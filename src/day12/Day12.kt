c784fc4bf2920eb0e705bbb71746d8ec92620150
Fabian <7341484+zypus@users.noreply.github.com>
2021-12-19 15:23:49 +0100

    Solution day 12 (part 2)

--- src/day12/Day12.kt
+++ src/day12/Day12.kt
@@ -39,9 +39,44 @@ data class Graph(val nodes: List<Node>, val edges: List<Pair<Node, Node>>) {
         }
     }
 
+    fun findPathsAllowDoubleVisit(currentPath: List<Node>, endNode: Node, doubleVisitNode: Node): List<List<Node>> {
+        val currentNode = currentPath.last()
+        return if (currentNode == endNode) {
+            listOf(currentPath)
+        } else {
+            neighborsOf(currentNode).filter { nextNode ->
+                nextNode.size == SIZE.LARGE || nextNode !in currentPath || (nextNode == doubleVisitNode && currentPath.count { it == nextNode } < 2)
+            }.flatMap { nextNode ->
+                findPathsAllowDoubleVisit(currentPath + nextNode, endNode, doubleVisitNode)
+            }
+        }
+    }
+
 }
 
 fun part1(input: List<String>): Int {
+    val (nodes, edges) = parseNodeAndEdges(input)
+    val graph = Graph(nodes.values.toList(), edges)
+
+    return graph.allPaths(nodes["start"]!!, nodes["end"]!!).size
+}
+
+fun part2(input: List<String>): Int {
+    val (nodes, edges) = parseNodeAndEdges(input)
+    val graph = Graph(nodes.values.toList(), edges)
+    val smallNodesNotStartOrEnd = nodes.values.filter { it.size == SIZE.SMALL && it.name !in listOf("start", "end") }
+
+    val startNode = nodes["start"]!!
+    val endNode = nodes["end"]!!
+    val singleVisitPaths = graph.allPaths(startNode, endNode)
+    val doubleVisitPaths = smallNodesNotStartOrEnd.flatMap { doubleVisitNode ->
+        graph.findPathsAllowDoubleVisit(listOf(startNode), endNode, doubleVisitNode)
+    }.distinct()
+
+    return (singleVisitPaths + doubleVisitPaths).distinct().size
+}
+
+private fun parseNodeAndEdges(input: List<String>): Pair<MutableMap<String, Node>, List<Pair<Node, Node>>> {
     val nodes = mutableMapOf<String, Node>()
     val edges = input.map {
         val (from, to) = it.split("-")
@@ -53,13 +88,7 @@ fun part1(input: List<String>): Int {
         }
         fromNode to toNode
     }
-    val graph = Graph(nodes.values.toList(), edges)
-
-    return graph.allPaths(nodes["start"]!!, nodes["end"]!!).size
-}
-
-fun part2(input: List<String>): Int {
-    return input.size
+    return Pair(nodes, edges)
 }
 
 fun main() = AoCTask("day12").run {
@@ -67,6 +96,9 @@ fun main() = AoCTask("day12").run {
     check(part1(testInput) == 10)
     check(part1(readTestInput(2)) == 19)
     check(part1(readTestInput(3)) == 226)
+    check(part2(testInput) == 36)
+    check(part2(readTestInput(2)) == 103)
+    check(part2(readTestInput(3)) == 3509)
 
     println(part1(input))
     println(part2(input))
